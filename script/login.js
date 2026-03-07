607ba543640241c54c5ccfc9c3510fb71aaf062f
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2026-03-07 16:27:01 +0600

    Login function implement

---
+++ script/login.js
@@ -0,0 +1,14 @@
+// window.location.href = "index.html";
+const demoUserName = "admin";
+const demoUserPassword = "admin123";
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const username = e.target.username.value;
+  const password = e.target.password.value;
+  if (username == demoUserName && password == demoUserPassword) {
+    console.log(true);
+    window.location.href = "index.html";
+  } else {
+    alert("Username and password not matched");
+  }
+};
